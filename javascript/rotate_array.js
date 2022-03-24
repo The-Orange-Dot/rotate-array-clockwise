6ea2db6324d6033eff22421c891927363feb1376
Hung-Le <theorangedot@hungs-mac-mini.lan>
2022-03-24 11:37:48 +0900

    Completed

--- javascript/rotate_array.js
+++ javascript/rotate_array.js
@@ -1,5 +1,12 @@
 function rotateArray(arr, k) {
-  // type your code here
+  let rotate = k % arr.length;
+
+  let newArray = [];
+
+  newArray.push(...arr.splice(arr.length - rotate));
+  newArray.push(...arr.splice(0, arr.length));
+
+  return newArray;
 }
 
 if (require.main === module) {
@@ -16,9 +23,27 @@ if (require.main === module) {
 
   console.log("Expecting: [1, 2, 3]");
   console.log("=>", rotateArray([1, 2, 3], 3));
+
+  console.log("");
+
+  console.log("Expecting: [4, 1, 2, 3]");
+  console.log("=>", rotateArray([1, 2, 3, 4], 41));
+
+  console.log("");
+
+  console.log("Expecting: [2, 3, 1]");
+  console.log("=>", rotateArray([1, 2, 3], 5));
 }
 
 module.exports = rotateArray;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+/**
+ * Initialize an empty array.
+ * use newArray.push(array.splice(arr.length - k)))
+ * use newArray.push(array.splice(0, arr.length - k))
+ * newArray.reverse()
+ * MAKE SURE TO RETURN
+ */
